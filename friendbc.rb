8d736ff4cfbcc35ebb5c7897384a5c3a090e2dfe
Raghu Betina <rbetina@github.com>
2015-03-31 16:56:55 -0500

    Initial commit

---
+++ friendbc.rb
@@ -0,0 +1,8 @@
+require 'open-uri'
+require 'json'
+
+# You can ignore the next two lines; it's a hack to prevent
+#   errors on certain machines
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
